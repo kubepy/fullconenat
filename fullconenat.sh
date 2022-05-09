9fcd1b27109b4a46e1ff3a05e05d20b70a347746
kubepy <43437760+kubepy@users.noreply.github.com>
2022-05-09 18:20:58 +0800

    Update fullconenat.sh

--- fullconenat.sh
+++ fullconenat.sh
@@ -6,6 +6,9 @@ sleep 5
 TUN_NETDEV=warpspeed
 EXT_NETDEV=enp1s0
 
+IP_NAT_RULES=`iptables-save -t nat |grep -Ee "(-i|-o) ($EXT_NETDEV|$TUN_NETDEV) -j FULLCONENAT"`
+echo "$IP_NAT_RULES" | sed "s#-A#-D#" | xargs -r -L1 iptables -t nat
+
 iptables -t nat -I POSTROUTING -o $EXT_NETDEV -j FULLCONENAT
 iptables -t nat -I PREROUTING -i $EXT_NETDEV -j FULLCONENAT
 iptables -t nat -I PREROUTING -i $TUN_NETDEV -j FULLCONENAT
